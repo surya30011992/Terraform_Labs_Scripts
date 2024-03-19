996c9c4073d577f6f721edcd501cfc84db3c62f1
surya.mohanty <mohantysuryanarayan.168@gmail.com>
2024-03-19 13:18:06 +0530

    jenkins_code

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "instance_ami" {
+    type = string
+    default = "ami-02d7fd1c2af6eead0"
+  
+}
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+  
+}
+variable "instance_key" {
+    type = string
+    default = "EC2"
+  
+}
+variable "instance_vpc_id" {
+    type = string
+    default = "vpc-021fe2a3484a75f30"
+  
+}
+
